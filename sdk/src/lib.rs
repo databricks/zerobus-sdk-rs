1058dcbf2b85e9db8b545dedd749fa02f0475306
elenagaljak-db <elena.galjak@databricks.com>
2025-10-08 13:20:09 +0200

    Windows support for tls certificates

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -44,7 +44,7 @@ use tracing::{debug, error, info, instrument, span, Level};
 #[cfg(target_os = "linux")]
 const DEFAULT_CERT_FILE: &str = "/etc/ssl/certs/ca-certificates.crt";
 
-#[cfg(not(target_os = "linux"))]
+#[cfg(target_os = "macos")]
 const DEFAULT_CERT_FILE: &str = "/etc/ssl/cert.pem";
 
 /// The type of the stream connection created with the server.
@@ -257,12 +257,21 @@ impl ZerobusSdk {
     async fn create_secure_channel_zerobus_client(&self) -> ZerobusResult<ZerobusClient<Channel>> {
         // ClientTlsConfig doesn't see true native roots by default, so we need to load them manually.
         // TODO: Use certificates provided by Databricks tls rust platform.
-        let pem = tokio::fs::read(DEFAULT_CERT_FILE)
-            .await
-            .map_err(|_| ZerobusError::FailedToEstablishTlsConnectionError)?;
-        let cert = Certificate::from_pem(pem);
+        let tls_config = {
+            #[cfg(target_os = "windows")]
+            {
+                ClientTlsConfig::new()
+            }
 
-        let tls_config = ClientTlsConfig::new().ca_certificate(cert);
+            #[cfg(not(target_os = "windows"))]
+            {
+                let pem = tokio::fs::read(DEFAULT_CERT_FILE)
+                    .await
+                    .map_err(|_| ZerobusError::FailedToEstablishTlsConnectionError)?;
+                let cert = Certificate::from_pem(pem);
+                ClientTlsConfig::new().ca_certificate(cert)
+            }
+        };
 
         let channel = Channel::from_shared(self.zerobus_endpoint.clone())
             .map_err(|_| ZerobusError::InvalidZerobusEndpointError(self.zerobus_endpoint.clone()))?
