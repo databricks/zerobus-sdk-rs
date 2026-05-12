7d4b6199e86d9a69152488de30c46670f16d8be7
elenagaljak-db <elena.galjak@databricks.com>
2026-05-12 11:46:47 +0200

    revert debug
    Signed-off-by: elenagaljak-db <elena.galjak@databricks.com>

--- rust/sdk/src/arrow_stream.rs
+++ rust/sdk/src/arrow_stream.rs
@@ -1579,7 +1579,7 @@ impl ZerobusArrowStream {
                         );
                         return Ok(());
                     }
-                    trace!(
+                    debug!(
                         current_ack = ack_offset,
                         target_offset = offset_to_wait,
                         "Waiting for more acks"
